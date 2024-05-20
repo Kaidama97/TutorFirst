2680ebaef52c9197aae97b5e34ab8f792658458a
Snowj00 <lumjohannsen@gmil.com>
2024-05-20 18:19:21 +0800

    newBranch

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Test</Text>
       <Text>Jo Was Here</Text>
+      <Text>New Branch</Text>
       <StatusBar style="auto" />
     </View>
   );
