4d4dc24b53c951f1aa30ccc9a80e96915c2de3a6
Snowj00 <lumjohannsen@gmil.com>
2024-07-13 23:05:18 +0800

    reuploaded createClasScreen

--- src/screens/main/createClassScreen/index.tsx
+++ src/screens/main/createClassScreen/index.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
-//import ClassForm from './components/classForm';
+import ClassForm from './components/classForm';
 
 const CreateClassScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
 
 
-//<ClassForm navigation = {navigation}/>
+
   return (
 
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{ flex: 1 }}>
       <ScrollView>
-        
+        <ClassForm navigation = {navigation}/>
       </ScrollView>
     </KeyboardAvoidingView>
   )
